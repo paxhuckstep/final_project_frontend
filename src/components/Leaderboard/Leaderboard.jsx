57bb0426ed4ebf4f928bfe88128d9ced16027937
paxhuckstep <paxhuckstep@gmail.com>
2025-05-21 11:34:12 -0600

    added sports leaderboard, made leaderboards more dynamic

--- src/components/Leaderboard/Leaderboard.jsx
+++ src/components/Leaderboard/Leaderboard.jsx
@@ -1,6 +1,17 @@
+import { useEffect, useState } from "react";
+import { getLeaderboardData } from "../../Utils/api";
 import "./Leaderboard.css";
 
-function Leaderboard({ leaderboardTitle, leaderboardData, highScoreName }) {
+function Leaderboard({ leaderboardTitle, highScoreName }) {
+  const [leaderboardData, setLeaderboardData] = useState([]);
+  useEffect(() => {
+    getLeaderboardData(highScoreName)
+      .then((data) => {
+        setLeaderboardData(data);
+      })
+      .catch(console.error);
+  }, []);
+
   //   console.log(leaderboardData);
   return (
     <table className="leaderboard">
@@ -9,7 +20,9 @@ function Leaderboard({ leaderboardTitle, leaderboardData, highScoreName }) {
         <tr className="leaderboard__row">
           <th className="leaderboard__head-data">Rank</th>
           <th className="leaderboard__head-data">Username</th>
-          <th className="leaderboard__head-data leaderboard__head-data_score">Score</th>
+          <th className="leaderboard__head-data leaderboard__head-data_score">
+            Score
+          </th>
         </tr>
       </thead>
       <tbody className="leaderboard__body">
--- src/components/Leaderboards/Leaderboards.jsx
+++ src/components/Leaderboards/Leaderboards.jsx
@@ -1,26 +1,17 @@
-import { useEffect, useState } from "react";
-import { getLeaderboardData } from "../../Utils/api";
 import Leaderboard from "../Leaderboard/Leaderboard";
 import "./Leaderboards.css";
 
 function Leaderboards() {
-  const [pokemonLeaderboardData, setPokemonLeaderboardData] = useState([]);
-
-  useEffect(() => {
-    getLeaderboardData("pokemonHighScore")
-      .then((data) => {
-        setPokemonLeaderboardData(data);
-      })
-      .catch(console.error);
-  }, []);
-  
   return (
     <div className="leaderboards">
       <h2 className="leaderboards__title">Leaderboards</h2>
       <Leaderboard
         leaderboardTitle={"Pokemon High Scores"}
-        leaderboardData={pokemonLeaderboardData}
         highScoreName={"pokemonHighScore"}
+      />
+      <Leaderboard
+        leaderboardTitle={"Sports High Scores"}
+        highScoreName={"sportsHighScore"}
       />
     </div>
   );
