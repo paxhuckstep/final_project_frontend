b5c4846e3b0574b4e770e58b8da8096a4aa9b3fb
paxhuckstep <paxhuckstep@gmail.com>
2025-05-25 00:01:45 -0600

    modalwithform says loading maybe

--- src/components/ModalWithForm/ModalWithForm.jsx
+++ src/components/ModalWithForm/ModalWithForm.jsx
@@ -1,3 +1,43 @@
+// import "./ModalWithForm.css";
+
+// function ModalWithForm({
+//   children,
+//   buttonText,
+//   title,
+//   onClose,
+//   onSubmit,
+//   onSwitch,
+//   switchText,
+//   errorMessage,
+// }) {
+//   return (
+//     <div className="modal">
+//       <div className="modal__content">
+//         <h2 className="modal__title">{title}</h2>
+//         <button onClick={onClose} type="button" className="modal__close" />
+//         <form className="modal__form" onSubmit={onSubmit}>
+//           {children}
+          
+//             <p className="modal__error">{errorMessage}</p>
+//           <div className="modal__buttons-container">
+
+
+//             <button type="submit" className="modal__submit">
+//               {buttonText}
+//             </button>
+//             <button type="button" onClick={onSwitch} className="modal__switch">
+//               {switchText}
+//             </button>
+//           </div>
+//         </form>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default ModalWithForm;
+
+import { useEffect, useState } from "react";
 import "./ModalWithForm.css";
 
 function ModalWithForm({
@@ -10,20 +50,31 @@ function ModalWithForm({
   switchText,
   errorMessage,
 }) {
+  const [isLoading, setIsLoading] = useState(false);
+  const submitText = isLoading ? "Loading..." : buttonText;
+
+  useEffect(() => {
+    setIsLoading(false);
+  }, [errorMessage, children]);
+
   return (
     <div className="modal">
       <div className="modal__content">
         <h2 className="modal__title">{title}</h2>
         <button onClick={onClose} type="button" className="modal__close" />
-        <form className="modal__form" onSubmit={onSubmit}>
+        <form
+          className="modal__form"
+          onSubmit={() => {
+            onSubmit();
+            setIsLoading(true);
+          }}
+        >
           {children}
-          
-            <p className="modal__error">{errorMessage}</p>
+
+          <p className="modal__error">{errorMessage}</p>
           <div className="modal__buttons-container">
-
-
             <button type="submit" className="modal__submit">
-              {buttonText}
+              {submitText}
             </button>
             <button type="button" onClick={onSwitch} className="modal__switch">
               {switchText}
